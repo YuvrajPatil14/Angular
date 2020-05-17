718a63cd9901389f88cb2f6fa519972fea7c6c5c
YuvrajPatil14 <yuvrajpatil1441998@gmail.com>
2020-05-17 16:26:58 +0530

    coursera mod1_sol

---
+++ mod1_sol/app.js
@@ -0,0 +1,49 @@
+(function () {
+    'use strict';
+    
+    angular.module('Mod1', [])
+    .controller('LunchCheckController', LunchCheckController);
+    
+    LunchCheckController.$inject = ['$scope'];
+    function LunchCheckController($scope) {
+      $scope.items = "";
+      $scope.result = "";
+      $scope.pos='err'
+      $scope.check = function () {
+          var lenght = $scope.items.length
+
+        if( lenght>0)
+      {
+        
+        var items =  $scope.items.split(',')
+        var ilength = items.length
+        items.forEach(item => {
+            if(!item){
+                ilength = ilength-1
+            }
+        });
+        if(ilength === 0){
+            $scope.result = "Please Enter data first"
+            return 
+        }
+        if(ilength>3)
+        {   
+            $scope.result = "Too much !"
+
+        }
+        else{
+            $scope.result = "Enjoy !"
+        
+        }
+        $scope.pos='res'
+      }
+      else{
+        $scope.result = "Please Enter data first"
+      }
+     
+      }
+        
+    }
+    
+    })();
+    
