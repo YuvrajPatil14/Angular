e75a0a8549dbbebc7b63fdbc6d4b7333e30e186b
YuvrajPatil14 <yuvrajpatil1441998@gmail.com>
2020-05-31 19:48:52 +0530

    coursera mod3_sol

---
+++ mod3_sol/app.js
@@ -0,0 +1,130 @@
+(function() {
+  "use strict";
+
+  angular
+    .module("mod3Sol", [])
+    .controller("MenuCategoriesController", MenuCategoriesController)
+    .factory("MenuFactory", MenuFactory)
+    .constant("ApiBasePath", "https://davids-restaurant.herokuapp.com")
+    .directive("foundItems", FoundItemsDirective);
+
+  function FoundItemsDirective() {
+    var ddo = {
+      templateUrl:"foundItems.html",
+      scope: {
+        workHere : '@work',
+        items: '<',
+        onRemove: '&',
+      },
+      controller: FoundItemsDirectiveController,
+      controllerAs: 'menu',
+      bindToController: true
+    };
+
+    return ddo;
+  }
+
+  function FoundItemsDirectiveController() {
+    var menu = this;
+  
+    // list.cookiesInList = function () {
+    //   for (var i = 0; i < list.items.length; i++) {
+    //     var name = list.items[i].name;
+    //     if (name.toLowerCase().indexOf("cookie") !== -1) {
+    //       return true;
+    //     }
+    //   }
+      
+  
+    //   return false;
+    // };
+  }
+  MenuCategoriesController.$inject = ["MenuFactory"];
+  function MenuCategoriesController(MenuFactory) {
+    var menu = this;
+    menu.item = "";
+    menu.work = 'please work';
+    menu.error = ''
+    var menuList = MenuFactory();
+    // menu.items="";
+    // console.log(menu);
+
+    menu.getAll = function() {
+      // console.log("called");
+      console.log(menu.item);
+
+      var promise = menuList.getAllItems();
+      promise
+        .then(function(response) {
+         // menu.items = response.data.menu_items;
+          var res = menuList.getNarrowedItems(
+            menu.item,
+            response.data.menu_items
+          );
+          if(res.length === 0)
+          {
+            menu.items = []
+            menu.error = 'Nothing found!!'
+          }
+          else{
+            menu.items = res
+            menu.error = ''
+          }
+          console.table( menu.items );
+        })
+        .catch(function(err) {
+          console.log("somethig went wrong");
+        });
+    };
+
+    menu.removeItem = function(itemIndex) {
+      menuList.removeItem(menu.items, itemIndex);
+      console.log("remove item controller");
+    };
+  }
+
+
+  function MenuCategoriesService($http, ApiBasePath) {
+    var service = this;
+    service.getAllItems = function() {
+      console.log("called getAllItems");
+
+      var response = $http({
+        method: "GET",
+        url: ApiBasePath + "/menu_items.json",
+      });
+      return response;
+    };
+
+    service.getNarrowedItems = function(query, list) {
+      // console.log('narrrooooo', query,list)
+      var narrowedList = []
+      if(!query){
+        return narrowedList
+      }
+      var q = query.toLowerCase();
+      narrowedList = list.filter(
+        (item) => item.description.toLowerCase().indexOf(q) > -1
+      );
+      return narrowedList;
+    };
+
+    service.removeItem = function(items, itemIndex) {
+      console.log("remove item service ");
+
+       items.splice(itemIndex, 1);
+    };
+  }
+  MenuFactory.$inject = ["$http", "ApiBasePath"];
+
+  function MenuFactory($http,ApiBasePath) {
+    var factory = function () {
+      return new MenuCategoriesService($http,ApiBasePath);
+    };
+  
+    return factory;
+  }
+
+
+
+})();
